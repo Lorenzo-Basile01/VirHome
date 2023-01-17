6c59af33933f34dce262e6c9d0c856891e49e9ca
Lorenzo-Basile01 <bsllnz01l07h501r@studium.unict.it>
2023-01-17 18:05:19 +0100

    commit interfaccia grafica

--- src/main/java/com/example/ui/VirHomeFrame.java
+++ src/main/java/com/example/ui/VirHomeFrame.java
@@ -4,18 +4,47 @@ import com.example.domain.VirHome;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class VirHomeFrame extends javax.swing.JFrame{
 
-    private JPanel VirHomeFrame;
-    private JTextField tfCodice;
-    private JTextField tfTelefono;
-    private JButton btnAccedi;
-    private JTextField tfDomanda;
-    private JLabel Domanda;
+    private javax.swing.JPanel VirHomeFrame;
+    private javax.swing.JButton btnAccedi;
+    private javax.swing.JLabel title;
+    private javax.swing.JTextField textField1;
 
     private VirHome virHome;
 
+    public VirHomeFrame() {
 
+        virHome = VirHome.getInstance();
+        this.title = new javax.swing.JLabel();
+        VirHomeFrame.setSize(200,800);
+        this.title.setFont(new java.awt.Font("JetBrains Mono",0,36));
+
+        btnAccedi.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    accediActionPerformed(e);
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            }
+        });
+    }
+
+    private void accediActionPerformed(ActionEvent e) throws Exception {
+        //new MainPanel(virHome);
+
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("VirHomeFrame");
+        frame.setContentPane(new VirHomeFrame().VirHomeFrame);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
 }
